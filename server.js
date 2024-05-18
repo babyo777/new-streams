f8fddaed68ad00d8cc3ed8ff8407461b8ca43d09
babyo77 <onlybabyyo7@gmail.com>
2024-05-18 15:14:34 +0530

    new

--- server.js
+++ server.js
@@ -3,33 +3,24 @@ const cors = require("cors");
 const ytdl = require("ytdl-core");
 const app = express();
 const port = process.env.PORT || 4000;
-const cluster = require("node:cluster");
-const os = require("os");
-const totalCpu = os.cpus().length;
 
-if (cluster.isPrimary) {
-  for (let i = 0; i < totalCpu; i++) {
-    cluster.fork();
-  }
-} else {
-  app.use(cors());
-  app.get("/", async (req, res) => {
-    try {
-      const id = req.query.url;
-      if (id && ytdl.validateID(id)) {
-        const info = await ytdl.getInfo(id);
-        res
-          .sendStatus(200)
-          .json({ url: info.formats.filter((i) => i.itag === 18)[0].url });
-      } else {
-        res.sendStatus(404).send();
-      }
-    } catch (error) {
-      res.sendStatus(500).json({ error: error });
+app.use(cors());
+app.get("/", async (req, res) => {
+  try {
+    const id = req.query.url;
+    if (id && ytdl.validateID(id)) {
+      const info = await ytdl.getInfo(id);
+      res
+        .sendStatus(200)
+        .json({ url: info.formats.filter((i) => i.itag === 18)[0].url });
+    } else {
+      res.sendStatus(404).send();
     }
-  });
+  } catch (error) {
+    res.sendStatus(500).json({ error: error });
+  }
+});
 
-  app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
-  });
-}
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
